1dc4748b7b561ebd65a756c932b7fd80164ef01b
noahblob <noahhagardent@gmail.com>
2023-07-14 20:39:31 +1200

    Made final changes to styling and added functionality to shorten link

--- url-shortener/src/App.js
+++ url-shortener/src/App.js
@@ -2,12 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import InputShortener from './InputShortener';
 import BackAnimation from './BackAnimation';
+import LinkResult from './LinkResult';
+import { useState } from 'react';
 
 function App() {
+  const [inputValue, setInputValue] = useState(''); 
+
   return (
     <div className="container">
-      <InputShortener />
+      <InputShortener  setInputValue={setInputValue} />
       <BackAnimation />
+      <LinkResult inputValue={inputValue} />
     </div>
   );
 }
